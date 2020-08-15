f85ece1358163091fe52a12a377602b0aa337191
MiThaik <thaikm4@gmail.com>
2020-08-15 11:53:54 +0630

    polish structure

--- app/src/main/java/com/padcmyanmar/padcx/padc_x_themovieapp_mtkm/data/vos/ProductionCountriesVO.kt
+++
@@ -1,8 +0,0 @@
-package com.padcmyanmar.padcx.padc_x_themovieapp_mtkm.data.vos
-
-import com.google.gson.annotations.SerializedName
-
-data class ProductionCountriesVO (
-    @SerializedName("iso_3166_1")var iso31661 : String? ="",
-    @SerializedName("name")var name : String? =""
-)
